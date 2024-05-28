ec12b9237dd01550aa0b8da82f74cc627a93fd87
LanAnh <anh.pham25@student.passerellesnumeriques.org>
2024-05-28 10:48:12 +0700

    Setup React Router

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,21 @@
+
+import { Route, BrowserRouter as Router, Switch } from "react-router-dom";
 import "./App.css";
 import Navbar from "./components/layout/Navbar";
 import Search from "./components/users/Search";
 const App = () => {
   return (
     <div className="App">
-      <Navbar />
-      <div className="container">
-        <h1>GitHub Users Data</h1>
-        <Search />
-      </div>
-    </div>
+      <Router>
+        <Navbar />
+        <div className="container">
+          <h1>GitHub Users Data</h1>
+          <Switch>
+            <Route exact path="/" component={Search} />
+          </Switch>
+        </div>
+      </Router>
+    </div>  
   );
 };
 export default App;
